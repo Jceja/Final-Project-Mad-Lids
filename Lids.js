addc3ac489024bc5da2529960c44468314259088
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-12-11 03:06:28 -0800

    Fix maybe

--- Lids.js
+++ Lids.js
@@ -19,8 +19,9 @@ function setStory(){
     + wordsB[2] + "out of a " + wordsB[3] + "stone." + wordsB[0] + ", had to find " + wordsB[4] +
     "and had found them at " + wordsB[5] + ",Then" + wordsB[4]+ "went out an got ready the mighty"
     + wordsB[5] + "and it's trusty" + wordsB[6]+ "He was set off to kill off" + wordsB[7] +
-     "as you get close to " + wordsB[8]+ ",Where it lives. As the monstar gets up to you get the mighty weapon and" +
-      wordsB[9] + "stap it in";
+     "as you get close to " + wordsB[8]+ ",Where it lives. As the monster gets up to you get the mighty weapon and" +
+      wordsB[9] + "stapping it in its heart. After it's death you were treated to a reward of"
+      + wordsB[10] + "When you go to the King, he gives you " + wordsB[11];
 
   var onestory = document.getElementById("storyA");
   onestory.innerHTML = fullstory;
