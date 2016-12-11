e9be5dcdf3115285d656b1e322dc9b26d55baad1
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-12-11 02:39:55 -0800

    fixing it

--- Lids.js
+++ Lids.js
@@ -1,4 +1,4 @@
-window.load = storySetting;
+window.onload = storySetting;
 function storyCreator(){
  var newButton = document.getElementById("buttonB");
  var result = newButton.link("Story.html");
@@ -9,15 +9,20 @@ function setStory(){
   var inputs = document.getElementsByTagName("input");
   for(i=0; i<inputs.length;i++){
     words[i] = inputs[i].value}
-    var story = words[0] + "is the ruler of" + words[1] + "he has been a ruller after he pulled"
-     + words[2] + "out of a " + words[3] + "stone." + words[0] + ", had to find " + words[4] +
-     "and had found them at " + words[5] + ",Then" + words[4]+ "went out an got ready the mighty"
-     + words[5] + "and it's trusty" + words[6]+ "He was set off to kill off" + words[7] + "as you get close to " + words[8]+ ",Where it lives. As the monstar gets up to you get the mighty weapon and" + words[9] + "stap it in";
-    setCookie("wordsofLids", story )
+    var groups = JSON.stringify(words);
+    createCookie('wordsofLids', groups )
    alert("Your words have been saved");
   }
  function storySetting(){
-  var fullstory =  getCookie("wordsofLids");
+     var fullstory =  getCookie('wordsofLids');
+     var wordsB = JSON.parse(fullstory);
+   var story = wordsB[0] + "is the ruler of" + wordsB[1] + "he has been a ruller after he pulled"
+    + wordsB[2] + "out of a " + wordsB[3] + "stone." + wordsB[0] + ", had to find " + wordsB[4] +
+    "and had found them at " + words[5] + ",Then" + words[4]+ "went out an got ready the mighty"
+    + wordsB[5] + "and it's trusty" + wordsB[6]+ "He was set off to kill off" + wordsB[7] +
+     "as you get close to " + wordsB[8]+ ",Where it lives. As the monstar gets up to you get the mighty weapon and" +
+      wordsB[9] + "stap it in";
+
   var onestory = document.getElementById("storyA");
   onestory.innerHTML = fullstory;
  }
