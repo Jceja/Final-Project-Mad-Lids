c27785d3b5236d4a73a073e0b540c8473ca892d8
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-12-11 19:36:22 -0800

    Last Finishes hopefully

--- Lids.js
+++ Lids.js
@@ -15,20 +15,21 @@ function setStory(){
  function storySetting(){
      var fullstory =  getCookie('wordsofLids');
      var wordsB = JSON.parse(fullstory);
-   var story = wordsB[0] + "is the ruler of" + wordsB[1] + "he has been a ruller after he pulled"
-    + wordsB[2] + "out of a " + wordsB[3] + "stone." + wordsB[0] + ", had to find " + wordsB[4] +
-    "and had found them at " + wordsB[5] + ",Then" + wordsB[4]+ "went out an got ready the mighty"
-    + wordsB[5] + "and it's trusty" + wordsB[6]+ "He was set off to kill off" + wordsB[7] +
-     "as you get close to " + wordsB[8]+ ",Where it lives. As the monster gets up to you get the mighty weapon and" +
-      wordsB[9] + "stapping it in its heart. After it's death you were treated to a reward of"
-      + wordsB[10] + "When you go to the King, he gives you " + wordsB[11];
-console.log( story);
-var span = document.getElementById("storyA");
- span.innerHTML = story;
+   var story = wordsB[0] + " is the ruler of " + wordsB[1] + ", he has been a ruller of " + wordsB[1] + " ,after he pulled the "
+    + wordsB[2] + " out of a " + wordsB[3] + " stone. " + wordsB[0] + " , had to find " + wordsB[4] +
+    " to go kill a "+ wordsB[8] +" at "+ wordsB[5] + " ,Then " + wordsB[4]+ " went out to get "
+    + wordsB[6] + " and it's trusty " + wordsB[7]+ " He was set off to kill off " + wordsB[8] +
+     " as you get close to " + wordsB[9]+ " ,Where it lives. As the monster gets up to you get the mighty weapon and " +
+      wordsB[10] + " stapping it in its heart. After it's death King " + wordsB[4] + " gave "
+      + wordsB[11] + " to " + wordsB[4];
+console.log(story);
+document.getElementById("storyA").innerHTML = story;
+}
 
 
- }
+ function allStory(){
 
+}
 //courtesy of W3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
