df37c64176a31d38692e68667203b9558898f349
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-12-12 18:36:43 -0800

    edited words

--- Lids.js
+++ Lids.js
@@ -21,12 +21,12 @@ function storySetting(){
   var fullstory =  getCookie('wordsofLids');
   var wordsB = JSON.parse(fullstory);
   var story = wordsB[0] + " is the ruler of " + wordsB[1] + ", he has been a ruller of " + wordsB[1] + " ,after he pulled the "
-    + wordsB[2] + " out of a " + wordsB[3] + " stone. King " + wordsB[0] + " , had to find " + wordsB[4] +
-    " to go kill a "+ wordsB[7] +" at "+ wordsB[8] + " ,Then " + wordsB[4]+ " went out to get a "
-    + wordsB[5] + " and it's trusty " + wordsB[6]+ ". He was set off to kill off the " + wordsB[9] +" "
-   + wordsB[7] + " ,When " +wordsB[4] +" found where it lives. The " + wordsB[7] + " gets up and try to attack "
-   + wordsB[4] + " But, " + wordsB[4] + " got his mighty " + wordsB[5] + " and " + wordsB[9] + " stapping it in its heart. After it's death. King " + wordsB[0] + " rewarded "
-      + wordsB[10] + " to " + wordsB[4];
+    + wordsB[2] + " out of a " + wordsB[3] + " stone. King " + wordsB[0] + " , had to find " + wordsB[4] + " at "
+    + wordsB[5]+ " to go kill a "+ wordsB[7] +" at "+ wordsB[8] + " ,Then " + wordsB[4]+ " went out to get a "
+    + wordsB[7] + " and it's trusty " + wordsB[6]+ ". He was set off to kill off the " + wordsB[8] +" at "
+   + wordsB[9] + " ,When " +wordsB[4] +" found where it lives. The " + wordsB[7] + " gets up and try to attack "
+   + wordsB[4] + " But, " + wordsB[4] + " got his mighty " + wordsB[8] + " and " + wordsB[10] + " stapping it in its heart. After it's death. King " + wordsB[0] + " rewarded "
+      + wordsB[11] + " to " + wordsB[4];
   console.log(story);
   document.getElementById("storyA").innerHTML = story;
 }
